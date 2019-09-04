ebdae491418842b8b77f0556e7a732fbd6108fd5
khazi <khazi@fzi.de>
2019-09-04 17:19:11 +0200

    f1

--- test_pro.srcs/sources_1/new/matMult.vhd
+++ test_pro.srcs/sources_1/new/matMult.vhd
@@ -52,18 +52,10 @@ end matMult;
 architecture Behavioral of matMult is
 constant  clkPeriod : time := 20ns;
 
-type state_type is (s0,s1);  --type of state machine.
+type state_type is (rstState,zeroState,padState,multState, addState);  --type of state machine.
 signal current_s,next_s: state_type;
 
 signal rst : STD_LOGIC:= '0';
-
---type t_2d_array is array(integer range <>, integer range <>) of  STD_LOGIC_VECTOR(3 downto 0);
---type t_2d_kernal is array(integer range <> , integer range <>) of STD_LOGIC_VECTOR(3 downto 0);
---type t_2d_out is array(integer range <>) of STD_LOGIC_VECTOR(7 downto 0);
---type t_tempKernal2d is array(integer range <>, integer range <>) of STD_LOGIC_VECTOR(7 downto 0);
-
-
-
 signal matKernal : t_2d_kernal(0 to kernRow-1, 0 to kernCol-1);
 
 signal filtKernal : t_2d_kernal(0 to kernRow-1, 0 to kernCol-1) := (("0001","0000","0000"),("0000","0001","0000"),("0000","0000","0001"));
@@ -84,12 +76,99 @@ signal temp: t_tempKernal2d(0 to kernRow-1, 0 to kernCol-1) := (("00000000","000
 
 signal padInp : t_2d_array(-1 to 4, -1 to 4); 
 signal zeroMat : STD_LOGIC := '0';
+signal doneMult : STD_LOGIC := '0';
+
+-- ************************************************* ARCHITECTURE********************************************************
 begin
+
+process (clk)
+begin
+ if (rst = '1') then
+  current_s <= rstState;  --default state on reset.
+elsif (rising_edge(clk)) then
+  current_s <= next_s;   --state change.
+end if;
+end process;
+
+
+-- state machine process
+
+process(clk,rst) 
+begin 
+
+case current_s is
+    when rstState =>
+    if (rst = '0') then
+        next_s <= zeroState;
+    else
+        next_s <= rstState;
+       
+    end if;
+    
+     when zeroState =>
+     if (rst = '0' and zeroMat = '1') then
+        next_s <= padState;
+           
+     elsif (rst = '0' and zeroMat = '0') then
+        next_s <= zeroState;
+     elsif (rst = '0' and doneBit = '1') then
+        next_s <= rstState; 
+     else
+         next_s <= rstState;
+     end if;
+    
+     when padState =>
+     if (rst = '0' and donePad = '1') then
+        next_s <= multState; 
+     elsif (rst = '0' and donePad = '0') then
+        next_s <= padState;
+     
+     elsif (rst = '0' and doneBit = '1') then
+        next_s <= rstState;  
+     else
+        next_s <= rstState;
+     end if;
+         
+     when multState =>
+     if (rst = '0' and doneMult = '1') then
+        next_s <= addState;
+        
+     elsif (rst = '0' and doneMult = '0') then
+        next_s <= multState;
+     
+     elsif (rst = '0' and doneBit = '1') then
+  
+        next_s <= rstState;
+     
+     else 
+        next_s <= rstState; 
+     end if;
+        
+        
+     when addState =>
+     if (rst = '0' and doneBit = '1') then
+   
+        next_s <= rstState;
+        
+     elsif (rst = '0' and doneBit = '0') then
+  
+        next_s <= multState;
+     else
+        next_s <= rstState;
+        
+     end if;
+ end case;     
+end process;
+
+
+-- Reset state process
 
 process(clk) 
 begin
 
 if rising_edge(clk) then
+if(current_s = zeroState) then
+if doneBit = '0' then
 for i in -1 to 4 loop
     for j in -1 to 4 loop
         padInp(i, j) <= "0000";
@@ -99,13 +178,15 @@ for i in -1 to 4 loop
                 zeroMat <= '0';
         end if; 
     end loop;
-  --zeroMat<= '1';
+  
 end loop;
+end if;
+end if;
 end if;
 
     
 if rising_edge(clk) then
-if zeroMat ='1' then
+if(current_s = padState) then
 for i in 0 to 3 loop
     for j in 0 to 3 loop
         padInp(i,j) <= inpMat(i,j);
@@ -117,14 +198,14 @@ for i in 0 to 3 loop
     end loop;
 end loop;
 end if;
-
+end if;
 
 
 --process(clk) is 
 --begin   
    if rising_edge(clk) then
-        --doneSig <= '0';
-        if(donePad ='1') then
+   if(current_s = multState) then
+        
            -- for i in (kernRow-matRow) to 1 loop
            --     for j in (kernCol-matCol) to 1 loop
             for i in 0 to 2 loop
@@ -136,10 +217,10 @@ end if;
                         multEnb <= '0';
                     end if;  
                 end loop; 
-            end loop;
-        end if;
+            end loop; 
+       
     end if;
-    end if;
+   end if;
     
 end process;   
   
@@ -148,78 +229,57 @@ process(clk) is
 
 begin
 if rising_edge(clk) then     
---    multEnb <= 1;
---    doneBit <= '0';
+
+--if(current_s = s2) then
     if(multEnb ='1') then
         for i in 0 to 2 loop 
             for j in 0 to 2 loop
         
                 temp(i,j) <=  matKernal(i,j) * filtKernal(i,j);
                 if(i=2 and j=2) then
-                addEnb <= '1';
+                doneMult <= '1';
                 else
-                addEnb <= '0';
+                doneMult <= '0';
                 end if;  
         end loop;
         end loop;
-        --addEnb <= 1;            
+                    
+end if;
     end if;
-                  
-    if(addEnb = '1') then
+end process;
+  
+  
+process(clk) is
+begin 
+
+if rising_edge(clk) then
+   if(current_s = addState) then 
         tempOut(z) <= temp(0,0) + temp(0,1) + temp(0,2) + temp(1,0) + temp(1,1) + temp(1,2) + temp(2,0) +temp(2,1) + temp(2,2);
-        doneSig <= '1';
+        --doneSig <= '1';
         if(z < 15) then
             z <= z + 1;
+            doneBit <= '0';
+            if(n < 3)  then      -- ((matRow-kernRow)/kernStride)) then --(matRow-kernRow)) then   -- n< 1
+                n <= n + 1;
+                m <= m;
+            elsif(m < 3) then --((matCol-kernCol)/kernStride)) then  -- (matRow-kernRow)) then -- -- m <1
+                n <= 0;
+                m <= m + 1;
+            else
+                n <= 0;
+                m <= 0;
+            end if;
         else
             z<= 0;
-             n <= 0;
-             m <= 0;
-            doneBit <= '1';
-            tempOut <= (x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00") after clkPeriod /2;
-        end if;
-       -- multEnb <= '0' after 10ns ;
-       -- addEnb <= '0' after 10ns;
-    end if;
-    
-   if(donePad ='1') then    
-    --((((matRow-kernRow)/kernStride)+1) * (((matRow-kernRow)/kernStride)+1)) -2) then -- (matRow-kernRow)+1) then -- -- z<=2
-        
-        if(n < 3)  then      -- ((matRow-kernRow)/kernStride)) then --(matRow-kernRow)) then   -- n< 1
-            n <= n + 1;
-            m <= m;
-        elsif(m < 3) then --((matCol-kernCol)/kernStride)) then  -- (matRow-kernRow)) then -- -- m <1
-            n <= 0;
-            m <= m + 1;
-        else
             n <= 0;
             m <= 0;
-       --multEnb <= '0';
-       --zeroMat <= '0';
+            tempOut <= (x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00") after clkPeriod /2;
+            doneBit <= '1';
         end if;
+     
     end if;
-    
-       
-   
-     doneSig <= '1';
-        --multEnb <= '0';
-            --tempOut <= (x"00",x"00",x"00",x"00") after clkPeriod /2;
-        -- tempOut <= (x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00",x"00") after clkPeriod /2;
-    --end if;
-       
---    if(n < 3)  then      -- ((matRow-kernRow)/kernStride)) then --(matRow-kernRow)) then   -- n< 1
---       n <= n + 1;
---       m <= m;
---    elsif(m < 3) then --((matCol-kernCol)/kernStride)) then  -- (matRow-kernRow)) then -- -- m <1
---       n <= 0;
---       m <= m + 1;
---    else
---       n <= 0;
---       m <= 0;
---       --multEnb <= '0';
---       --zeroMat <= '0';
---    end if;
+    end if;
               
-end if;
 
 end process;
    
